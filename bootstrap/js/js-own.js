59305d0e909f7b471f5283abeec1850856d74d31
antonio-1992 <antonio.muniz.3030@gmail.com>
2022-04-05 15:27:48 -0500

    primer comentario

---
+++ bootstrap/js/js-own.js
@@ -0,0 +1,6 @@
+$(".navbar ul a").on("click", function (e) {
+  e.preventDefault();
+  
+  const href = $(this).attr("href");
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
